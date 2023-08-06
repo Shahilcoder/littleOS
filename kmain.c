edb939ff4c61c1a4ff4ff6fdc52340071ab58687
Shahilcoder <shahilcoder@gmail.com>
2023-08-06 21:19:57 +0530

    Framebuffer feature

--- kmain.c
+++ kmain.c
@@ -1,2 +1,23 @@
-void kmain() {
+typedef unsigned char byte;
+
+typedef struct fb {
+    byte ch;
+    byte bgfg;
+} __attribute__((packed)) fb;
+
+fb *fbp = (fb*)0x000B8000;
+
+/** fb_write_cell:
+* Writes a character with the given foreground and background to position i
+* in the framebuffer.
+*
+* @param i The location in the framebuffer
+* @param c The character
+* @param fg The foreground color
+* @param bg The background color
+*/
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg) {
+    fbp[i].bgfg = ((bg & 0x0F) << 4) | (fg & 0x0F);
+    fbp[i].ch = c;
 }
--- loader.s
+++ loader.s
@@ -1,9 +1,15 @@
 global loader                         ; the entry symbol for ELF
 
+KERNEL_STACK_SIZE equ 4096            ; size of stack in bytes
 MAGIC_NUMBER equ 0x1BADB002           ; define the magic number constant
 FLAGS        equ 0x0                  ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER        ; calculate the checksum
                                       ; (magic number + checksum + flags = 0)
+
+section .bss
+align 4                               ; the code must be 4 byte aligned
+kernel_stack:
+    resb KERNEL_STACK_SIZE
 
 section .text:                        ; start of the text (code) section
 align 4                               ; the code must be 4 byte aligned
@@ -11,7 +17,62 @@ align 4                               ; the code must be 4 byte aligned
     dd FLAGS                          ; the flags
     dd CHECKSUM                       ; and the checksum
 
-loader:
-    mov eax, 0xCAFEBABE              ; the loader label (defined as entry point in linker script)
+loader:                               ; the loader label (defined as entry point in linker script)
+    extern fb_write_cell
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
+    mov eax, 0xCAFEBABE
+
+    ; write SHAHIL with help of C function
+    push byte 0x00
+    push byte 0x04
+    push byte 0x53
+    push dword 0x00000050
+
+    call fb_write_cell
+
+    push byte 0x00
+    push byte 0x04
+    push 0x48
+    push dword 0x00000051
+
+    call fb_write_cell
+
+    push byte 0x00
+    push byte 0x04
+    push 0x41
+    push dword 0x00000052
+
+    call fb_write_cell
+
+    push byte 0x00
+    push byte 0x04
+    push 0x48
+    push dword 0x00000053
+
+    call fb_write_cell
+
+    push byte 0x00
+    push byte 0x04
+    push 0x49
+    push dword 0x00000054
+
+    call fb_write_cell
+
+    push byte 0x00
+    push byte 0x04
+    push 0x4C
+    push dword 0x00000055
+
+    call fb_write_cell
+
+    ; Write HUSSAIN on the frame buffer via Assembly
+    mov [0x000B80AE], word 0x0448         ; access the framebuffer via Memory mapped io, and write 'H' with green color and dark grey background
+    mov [0x000B80B0], word 0x0455         ; write second cell (after 4 bits) [:( I dunno how it works] U
+    mov [0x000B80B2], word 0x0453         ; write S
+    mov [0x000B80B4], word 0x0453         ; write S
+    mov [0x000B80B6], word 0x0441         ; write A
+    mov [0x000B80B8], word 0x0449         ; write I
+    mov [0x000B80BA], word 0x044E         ; write N
+
 .loop:
     jmp .loop                         ; loop forever
