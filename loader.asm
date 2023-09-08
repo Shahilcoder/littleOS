115bc37895a73e4f2bbda50a4ba411bb4e487383
Shahilcoder <shahilcoder@gmail.com>
2023-09-08 14:20:01 +0530

    Switch to Protected mode

--- loader.asm
+++ loader.asm
@@ -49,13 +49,13 @@ GetMemInfo:
     jnz GetMemInfo
 
 GetMemDone:
-    mov ah, 0x13
-    mov al, 1
-    mov bx, 0xa
-    xor dx, dx
-    mov bp, Message
-    mov cx, MessageLen
-    int 0x10
+    ; mov ah, 0x13
+    ; mov al, 1
+    ; mov bx, 0xa
+    ; xor dx, dx
+    ; mov bp, Message
+    ; mov cx, MessageLen
+    ; int 0x10
 
 TestA20:
     mov ax, 0xffff
@@ -74,21 +74,16 @@ SetA20LineDone:
 SetVideoMode:
     mov ax, 3
     int 0x10
+    cli ; clear interrupt
+    lgdt [Gdt32Ptr]
+    lidt [Idt32Ptr]
 
-    mov si, Message
-    mov ax, 0xb800
-    mov es, ax
-    xor di, di
-    mov cx, MessageLen
+    ; enable protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
 
-PrintMessage:
-    mov al, [si]
-    mov [es:di], al
-    mov byte[es:di+1], 0xa
-
-    add di, 2
-    add si, 1
-    loop PrintMessage ; cx register as the counter
+    jmp 8:PMEntry
 
 ReadError:
 NotSupport:
@@ -96,10 +91,49 @@ End:
     hlt
     jmp End
 
+[BITS 32]
+
+PMEntry:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov esp, 0x7c00
+
+    mov byte[0xb8000], 'P'
+    mov byte[0xb8001], 0xa
+
+PEnd:
+    hlt
+    jmp PEnd
+
 DriveId:
     db 0
-Message:
-    db "Text mode is set"
-MessageLen equ $-Message
 ReadPacket:
     times 16 db 0
+
+Gdt32:
+    dq 0
+Code32:
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x9a
+    db 0xcf
+    db 0
+Data32:
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x92
+    db 0xcf
+    db 0
+
+Gdt32Len equ $-Gdt32
+Gdt32Ptr:
+    dw Gdt32Len - 1
+    dd Gdt32
+
+Idt32Ptr:
+    dw 0
+    dd 0
